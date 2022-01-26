8fcf5eba3c148a85a252dd14b05157ebc66b4e1d
Ryan Chandler <ryangjchandler@gmail.com>
2022-01-26 11:37:58 +0000

    chore: tidy up test helpers

--- tests/raw-echo/raw-echo.test.ts
+++ tests/raw-echo/raw-echo.test.ts
@@ -1,16 +1,8 @@
 import fs from "fs";
 import path from "path";
-import prettier from "prettier";
-import plugin from "../../src/plugin";
+import { format } from "../utils";
 
 const fixture = fs.readFileSync(path.join(__dirname, "./raw-echo.blade.php"), "utf-8");
-
-function format(content: string) {
-    return prettier.format(content, {
-        parser: "blade",
-        plugins: [plugin as any as string]
-    });
-}
 
 test("basic", () => {
     const formatted = format(fixture);
---
+++ tests/utils.ts
@@ -0,0 +1,9 @@
+import prettier from "prettier";
+import plugin from "../src/plugin";
+
+export function format(content: string) {
+    return prettier.format(content, {
+        parser: "blade",
+        plugins: [plugin as any as string]
+    });
+}
