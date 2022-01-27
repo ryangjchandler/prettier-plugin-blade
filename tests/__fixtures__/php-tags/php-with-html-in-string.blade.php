bf529e0f107f77063a5aa86c82224d4dd1fdb68f
Oliver Nybroe <oliver@worksome.com>
2022-01-27 14:23:37 +0100

    tests(php): Add failing tests for php string with html inside it

---
+++ tests/__fixtures__/php-tags/php-with-html-in-string.blade.php
@@ -0,0 +1,10 @@
+<?
+    echo "<title>Some to be happy about</title>"
+    ?>
+<h1>Awesome
+</h1>
+----
+@php
+    echo "<title>Some to be happy about</title>";
+@endphp
+<h1>Awesome</h1>
--- tests/fixtures.test.ts
+++ tests/fixtures.test.ts
@@ -2,14 +2,13 @@ import { allFilesIn, format, readFile } from "./utils";
 import { join } from "path";
 
 test.each(
-  allFilesIn(join(__dirname, "/", "__fixtures__"))
-    .map(function(path) {
-      return {
-        path,
-        name: path.replace(join(__dirname, "/", "__fixtures__", "/"), "")
-      }
-    })
-)('$name', ({name, path}) => {
+  allFilesIn(join(__dirname, "/", "__fixtures__")).map(function (path) {
+    return {
+      path,
+      name: path.replace(join(__dirname, "/", "__fixtures__", "/"), ""),
+    };
+  })
+)("$name", ({ name, path }) => {
   const [original, expected] = readFile(path)
     .split("----")
     .map((part) => part.trimStart());
