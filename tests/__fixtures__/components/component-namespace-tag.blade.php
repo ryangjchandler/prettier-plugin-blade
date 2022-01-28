8259170c2e8c5e07251708f055e991d5edc47338
Julius Kiekbusch <jubeki99@gmail.com>
2022-01-28 11:45:37 +0100

    Fix whitespace

--- tests/__fixtures__/components/component-namespace-tag.blade.php
+++ tests/__fixtures__/components/component-namespace-tag.blade.php
@@ -5,11 +5,10 @@
         Default Slot</div>
         </x-view>
 ----
-<x-view x="X" :y="'Y'">
+<x-namespace::view x="X" :y="'Y'">
     <x-slot name="Name">
         <div>Named Slot</div>
     </x-slot>
     
     <div>Default Slot</div>
-</x-view>
-        
+</x-namespace::view>
