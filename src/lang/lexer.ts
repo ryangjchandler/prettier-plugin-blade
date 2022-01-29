4a1d061b6a1aef0274d4106ca716c72af869b152
Ben James <in@benjam.es>
2022-01-29 21:36:43 +0000

    Fixes a couple of tests. Replaces short php now, along with only removing the closing php tag on its own line. Preventing the string replace of the closing echo statement tag
    Signed-off-by: Ben James <in@benjam.es>

--- src/lang/lexer.ts
+++ src/lang/lexer.ts
@@ -14,8 +14,7 @@ export class Lexer {
     private buffer: string = "";
     constructor(source: string) {
         this.source = source
-            .replace("<?php", "@php")
-            .replace("?>", "@endphp")
+            .replace(/(\<\?(php)?\n)(.+)(\n\s+\?\>)/gm, "@php$3 @endphp")
             .replace(/\r\n|\r|\n/, "\n")
             .split("");
     }
