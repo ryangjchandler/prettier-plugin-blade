3f37aaaeb63ba3e348ef336ee5e660f07a5a800f
claytonrcarter <claytonrcarter@gmail.com>
2022-03-11 16:18:17 -0500

    fix(plugin): passthrough prettier options into html/php formatters (closes #41) (#49)

--- src/utils.ts
+++ src/utils.ts
@@ -6,9 +6,32 @@ const tw = require("prettier-plugin-tailwindcss");
 const htmlDebug = false;
 const phpDebug = false;
 
-let pluginOptions: ParserOptions;
+let htmlOptions: ParserOptions;
+let phpOptions: ParserOptions;
 export const setOptions = (options: ParserOptions) => {
-    pluginOptions = options;
+    htmlOptions = Object.assign({}, options, {
+        parser: "html",
+        plugins: [{ parsers: { html: tw.parsers.html } }],
+    });
+
+    phpOptions = Object.assign({}, options, { parser: "php", plugins: [php] });
+
+    // FIXME there *must* be a better way to get just the user configurable options?!
+    [
+        "cursorOffset",
+        "rangeEnd",
+        "rangeStart",
+        "locEnd",
+        "locStat",
+        "printer",
+        "originalText",
+        "astFormat",
+    ].forEach((p) => {
+        // @ts-ignore
+        delete htmlOptions[p];
+        // @ts-ignore
+        delete phpOptions[p];
+    });
 };
 
 export const formatAsHtml = (source: string): string => {
@@ -18,11 +41,7 @@ export const formatAsHtml = (source: string): string => {
     htmlDebug && debugOutput.push(`source:    '${source}'`);
 
     try {
-        formatted = format(source, {
-            parser: "html",
-            plugins: [{ parsers: { html: tw.parsers.html } }],
-            tabWidth: pluginOptions?.tabWidth,
-        });
+        formatted = format(source, htmlOptions);
     } catch (e) {
         htmlDebug && debugOutput.push("error: defaulting to source");
 
@@ -56,7 +75,7 @@ export const formatAsPhp = (source: string): string => {
     phpDebug && debugOutput.push(`manipulated: '${manipulated}'`);
 
     try {
-        formatted = format(manipulated, { parser: "php", plugins: [php] })
+        formatted = format(manipulated, phpOptions)
             .replace("<?php ", "")
             .trim();
     } catch (e) {
