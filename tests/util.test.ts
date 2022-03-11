f6e79992c26eb727880501d3c2249f9b3238b769
claytonrcarter <claytonrcarter@gmail.com>
2022-03-11 16:17:28 -0500

    feat: support for echos in HTML elements using placeholder strings (closes #45) (#48)

--- tests/util.test.ts
+++ tests/util.test.ts
@@ -1,4 +1,4 @@
-import { placeholderElement } from "../src/utils";
+import { placeholderElement, placeholderString } from "../src/utils";
 
 describe("placeholderElement", () => {
     it("has a minimum length", () => {
@@ -6,14 +6,32 @@ describe("placeholderElement", () => {
         let placeHolder = placeholderElement("foo", content);
 
         expect(content.length).toBeLessThan(placeHolder.length);
-        expect(placeHolder).toMatch(/<foo-\d \/>/);
+        expect(placeHolder).toMatch(/<foo-. \/>/);
     });
 
     it("pads output to match length of content", () => {
         let content = "{{ $thisHasLength22 }}";
         let placeHolder = placeholderElement("foo", content);
 
-        expect(content.length).toBe(placeHolder.length);
-        expect(placeHolder).toMatch(/<foo-\d-x+ \/>/);
+        expect(placeHolder.length).toBe(content.length);
+        expect(placeHolder).toMatch(/<foo-.-x+ \/>/);
+    });
+});
+
+describe("placeholderString", () => {
+    it("has a minimum length", () => {
+        let content = "1";
+        let placeHolder = placeholderString("foo", content);
+
+        expect(content.length).toBeLessThan(placeHolder.length);
+        expect(placeHolder).toMatch(/__foo_./);
+    });
+
+    it("pads output to match length of content", () => {
+        let content = "{{ $thisHasLength22 }}";
+        let placeHolder = placeholderString("foo", content);
+
+        expect(placeHolder.length).toBe(content.length);
+        expect(placeHolder).toMatch(/__foo_._x+/);
     });
 });
